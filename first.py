9ba02843c836a59315166a74f9bb1bf84460cb9c
so2emma <67445533+so2emma@users.noreply.github.com>
2021-06-22 20:22:31 +0100

    first uploaded python code

---
+++ first.py
@@ -0,0 +1,31 @@
+# my first python code
+# section 1
+print("Hello python world")
+
+# section 2
+name = "oso emmanuel"
+print(name.title())
+
+# section 3
+first_name = "oso"
+last_name = "emmanuel"
+full_name = first_name + " " + last_name
+print(full_name)
+
+# section 4
+print(3 + 4)
+print(9 - 2)
+print(7 * 56)
+print(8 / 76)
+
+# section 4
+# working with lists
+names = ["james", "samuel", "moses", "faith", "jude"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+# append
+names.append("samson")
+print(names)
