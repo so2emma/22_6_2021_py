3ef03a2ad354211951451f5f43dd2d1df39187fb
so2emma <67445533+so2emma@users.noreply.github.com>
2021-06-25 23:49:29 +0100

    Add files via upload

---
+++ Alien.py
@@ -0,0 +1,32 @@
+# assigning color of alien
+alien_color = "green"
+# correct condition
+if alien_color == "green":
+    print("You just earned 5 points")
+
+#  incorrect condition
+if alien_color != "green":
+    print()
+
+
+# if-else condition
+if alien_color == "green":
+    print("You just earned 5 points")
+else:
+    print("You just earned 10 points")
+
+# if-else condition
+if alien_color != "green":
+    print("You just earned 10 points")
+else:
+    print("You just earned 5 points")
+
+# if-elif condition
+if alien_color == "green":
+    print("You just earned 5 points")
+elif alien_color == "Yellow":
+    print("You just earned 5 points")
+elif alien_color == "":
+    print("You just earned 5 points")
+
+
---
+++ stage_of_life.py
@@ -0,0 +1,13 @@
+# if-elif-else condition
+if age < 2:
+    print("You are a baby")
+elif 2 <= age < 4:
+    print("you are a toddler")
+elif 4 <= age < 13:
+    print("you are a kid")
+elif 13 <= age < 20:
+    print("you are a teenager")
+elif 20 <= age < 65:
+    print("you are an adult")
+elif age >= 65:
+    print("you are an elder")
